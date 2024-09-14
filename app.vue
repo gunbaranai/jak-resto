bb30efc7ecdf7fde1284af374944bbb7b68fe68e
gunbaranai <ai.mo7g@gmail.com>
2024-09-14 14:10:35 +0700

    Initial commit. Tailwind and leaflet boilerplate.

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const dataFetch = await $fetch('https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/3100/var/1207/key/11e3953affcb8e0a7a1d0cb996343eb2')
+</script>
+
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <NuxtWelcome />
+  </div>
+</template>
